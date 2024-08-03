7b84fa0218bf81dbbde2865213c9d69ab44485d8
Marek <marek@MacBook-Air-Marek.local>
2024-08-03 20:37:02 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Script from "next/script";
 import Head from "next/head";
 import Header from "./components/header/header";
 import View from "./components/mainview/view";
@@ -10,6 +11,15 @@ import Contactform from "./components/contactfrom/contactform";
 export default function Home() {
   return (
     <>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-NNW1E1J5PY"></script>
+      <Script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-NNW1E1J5PY');`}
+      </Script>
       <Head>
         <meta property="Nieruchomości w Hiszpanii, Chorwacji, Portugalii" content="image" />
         <title>Biuro nieruchomości Namysłów. Biuro nieruchomości Syców.</title>
@@ -20,6 +30,10 @@ export default function Home() {
         <meta
           name="Description"
           content="Mieszkania i domy na terenie Namysłówa, Sycowa, Kluczborka, Oławy, Wołczyna oraz pozostałej części woj. opolskiego oraz wielkopolskiego."
+        />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, minimum-scale=1, maximum-scale=1"
         />
       </Head>
       <main className="text-black relative">
