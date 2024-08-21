61b39f79789ea07b376db95e329b472385ff0e4e
Marek <marek@MacBook-Air-Marek.local>
2024-08-21 19:12:54 +0200

    first commit

--- app/components/osiedleLesne/video.tsx
+++ app/components/osiedleLesne/video.tsx
@@ -17,10 +17,11 @@ export default function Video() {
       <div className="lg:w-[1000px] lg:h-[563px] md:w-[730px] md:h-[410px] sm:w-[600px] sm:h-[337px] w-[340px] h-[191px] bg-gray-300 mx-auto left-0 right-0 z-10 my-auto top-0 bottom-0  rounded-xl overflow-hidden">
         <div className="relative w-full h-full">
           <iframe
+            loading="lazy"
             width="100%"
             height="100%"
             src="https://www.youtube.com/embed/uhmTYJceekA?si=Cdhx1kc2gJOyo8o6&controls=1&playlist=uhmTYJceekA"
-            title="YouTube video player"
+            title="Nieruchomości Syców"
             // frameBorder={true}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; frameborder"
             // referrerpolicy="strict-origin-when-cross-origin"
