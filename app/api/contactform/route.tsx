762b0c9300659a2d6b5ca9c5d8e3aceb80a61b86
Marek <marek@MacBook-Air-Marek.local>
2024-08-26 16:22:52 +0200

    first commit

--- app/api/contactform/route.tsx
+++ app/api/contactform/route.tsx
@@ -7,7 +7,7 @@ export async function POST(req: any) {
   const fromEmail = process.env.FROM_EMAIL;
   const pass = process.env.EMAIL_PASS;
 
-  const transporter =  nodemailer.createTransport({
+  const transporter = await nodemailer.createTransport({
     port: 465,
     host: "mail-serwer141299.lh.pl",
     secure: true,
@@ -19,6 +19,19 @@ export async function POST(req: any) {
       user: fromEmail,
       pass: pass,
     },
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error: any, success: any) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
   });
 
   const mailData = {
